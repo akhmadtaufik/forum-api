73e9c42f3c77db082cd9698d45b7db0814edfd12
Akhmad Taufik <akhmadtaufik1703@gmail.com>
2025-05-16 19:18:43 +0700

    feat(comment): add HTTP interface for adding and managing comments
    - Add handler for posting new comments to a thread
    - Define route and plugin registration for /comments endpoint
    - Integrate with AddCommentUseCase for business logic

---
+++ src/Interfaces/http/api/comments/routes.js
@@ -0,0 +1,14 @@
+const { options } = require("@hapi/hapi/lib/cors");
+
+const rouutes = (handler) => [
+  {
+    method: "POST",
+    path: "/threads/{threadId}/comments",
+    handler: (request, h) => handler.postCommentHandler(request, h),
+    options: {
+      auth: "forumapi_jwt",
+    },
+  },
+];
+
+module.exports = rouutes;
