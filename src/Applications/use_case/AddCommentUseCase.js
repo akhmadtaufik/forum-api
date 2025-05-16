a6ae4091d992faec60234036c346e1d5a4baa6fb
Akhmad Taufik <akhmadtaufik1703@gmail.com>
2025-05-16 19:35:24 +0700

    fix(comment): handle not found error when thread does not exist
    - Add explicit NotFoundError for invalid thread ID before adding comment
    - Prevent generic error leakage from repository layer

--- src/Applications/use_case/AddCommentUseCase.js
+++ src/Applications/use_case/AddCommentUseCase.js
@@ -1,4 +1,5 @@
 const NewComment = require("../../Domains/comments/entities/NewComment");
+const NotFoundError = require("../../Commons/exceptions/NotFoundError");
 
 class AddCommentUseCase {
   constructor({ commentRepository, threadRepository }) {
@@ -7,11 +8,20 @@ class AddCommentUseCase {
   }
 
   async execute(useCasePayload, threadId, owner) {
-    await this._threadRepository.verifyThreadExists(threadId);
-
     const newComment = new NewComment(useCasePayload);
 
+    // Verify thread exists before adding comment
+    await this._verifyThreadExists(threadId);
+
     return this._commentRepository.addComment(newComment, threadId, owner);
+  }
+
+  async _verifyThreadExists(threadId) {
+    try {
+      await this._threadRepository.verifyThreadExists(threadId);
+    } catch (error) {
+      throw new NotFoundError("Thread tidak ditemukan");
+    }
   }
 }
 
