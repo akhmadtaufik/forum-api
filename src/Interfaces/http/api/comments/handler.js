e8eb7f8d7a0af851e258cafb86f1d3ad9706ce73
Akhmad Taufik <akhmadtaufik1703@gmail.com>
2025-05-19 17:01:11 +0700

    feat(comment): implement deleteCommentHandler for DELETE /threads/{threadId}/comments/{commentId} - bind deleteCommentHandler method to CommentsHandler context - inject and execute DeleteCommentUseCase with threadId, commentId, and ownerId - return success response after soft deleting a comment

--- src/Interfaces/http/api/comments/handler.js
+++ src/Interfaces/http/api/comments/handler.js
@@ -1,9 +1,11 @@
 const AddCommentUseCase = require("../../../../Applications/use_case/AddCommentUseCase");
+const DeleteCommentUseCase = require("../../../../Applications/use_case/DeleteCommentUseCase");
 
 class CommentsHandler {
   constructor(container) {
     this._container = container;
     this.postCommentHandler = this.postCommentHandler.bind(this);
+    this.deleteCommentHandler = this.deleteCommentHandler.bind(this);
   }
 
   async postCommentHandler(request, h) {
@@ -27,6 +29,21 @@ class CommentsHandler {
 
     response.code(201);
     return response;
+  }
+
+  async deleteCommentHandler(request, h) {
+    const { threadId, commentId } = request.params;
+    const { id: ownerId } = request.auth.credentials;
+
+    const deleteCommentUseCase = this._container.getInstance(
+      DeleteCommentUseCase.name
+    );
+
+    await deleteCommentUseCase.execute(threadId, commentId, ownerId);
+
+    return {
+      status: "success",
+    };
   }
 }
 
