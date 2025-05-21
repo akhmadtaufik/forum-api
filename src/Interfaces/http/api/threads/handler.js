feb4996f75b15643a65dedf4f6887020bccf26b0
Akhmad Taufik <akhmadtaufik1703@gmail.com>
2025-05-21 12:32:23 +0700

    feat(thread): add getThreadDetailHandler to return thread with comments - integrate GetThreadDetailUseCase into ThreadsHandler - bind getThreadDetailHandler and implement GET /threads/{threadId} logic

--- src/Interfaces/http/api/threads/handler.js
+++ src/Interfaces/http/api/threads/handler.js
@@ -1,9 +1,11 @@
 const AddThreadUseCase = require("../../../../Applications/use_case/AddThreadUseCase");
+const GetThreadDetailUseCase = require("../../../../Applications/use_case/GetThreadDetailUseCase");
 
 class ThreadsHandler {
   constructor(container) {
     this._container = container;
     this.postThreadHandler = this.postThreadHandler.bind(this);
+    this.getThreadDetailHandler = this.getThreadDetailHandler.bind(this);
   }
 
   async postThreadHandler(request, h) {
@@ -23,6 +25,22 @@ class ThreadsHandler {
 
     response.code(201);
     return response;
+  }
+
+  async getThreadDetailHandler(request, h) {
+    const { threadId } = request.params;
+    const getThreadDetailUseCase = this._container.getInstance(
+      GetThreadDetailUseCase.name
+    );
+
+    const thread = await getThreadDetailUseCase.execute(threadId);
+
+    return {
+      status: "success",
+      data: {
+        thread,
+      },
+    };
   }
 }
 
