d33ae0c9e6c9260855ac458769b8a627ea9d5e40
Akhmad Taufik <akhmadtaufik1703@gmail.com>
2025-05-21 19:49:44 +0700

    refactor(comment): simplify thread existence check in AddCommentUseCase - remove unnecessary _verifyThreadExists wrapper method - directly call threadRepository.verifyThreadExists and let it handle error

--- src/Applications/use_case/AddCommentUseCase.js
+++ src/Applications/use_case/AddCommentUseCase.js
@@ -1,5 +1,4 @@
 const NewComment = require("../../Domains/comments/entities/NewComment");
-const NotFoundError = require("../../Commons/exceptions/NotFoundError");
 
 class AddCommentUseCase {
   constructor({ commentRepository, threadRepository }) {
@@ -11,17 +10,9 @@ class AddCommentUseCase {
     const newComment = new NewComment(useCasePayload);
 
     // Verify thread exists before adding comment
-    await this._verifyThreadExists(threadId);
+    await this._threadRepository.verifyThreadExists(threadId);
 
     return this._commentRepository.addComment(newComment, threadId, owner);
-  }
-
-  async _verifyThreadExists(threadId) {
-    try {
-      await this._threadRepository.verifyThreadExists(threadId);
-    } catch (error) {
-      throw new NotFoundError("Thread tidak ditemukan");
-    }
   }
 }
 
