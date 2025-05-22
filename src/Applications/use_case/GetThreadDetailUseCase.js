8287d1321f657b99b0382a1bc7c1ad635642a090
Akhmad Taufik <akhmadtaufik1703@gmail.com>
2025-05-22 11:16:58 +0700

    feat(use-case): include replies when retrieving thread details
    - Injected replyRepository into GetThreadDetailUseCase
    - Retrieved replies for each comment using comment IDs
    - Integrated ReplyDetail entity into each CommentDetail
    - Returned enriched comments with nested replies in ThreadDetail

--- src/Applications/use_case/GetThreadDetailUseCase.js
+++ src/Applications/use_case/GetThreadDetailUseCase.js
@@ -1,44 +1,61 @@
 const ThreadDetail = require("../../Domains/threads/entities/ThreadDetail");
 const CommentDetail = require("../../Domains/comments/entities/CommentDetail");
+const ReplyDetail = require("../../Domains/replies/entities/ReplyDetail");
 
 class GetThreadDetailUseCase {
-  constructor({ threadRepository, commentRepository }) {
+  constructor({ threadRepository, commentRepository, replyRepository }) {
     this._threadRepository = threadRepository;
     this._commentRepository = commentRepository;
+    this._replyRepository = replyRepository;
   }
 
   async execute(threadId) {
+    await this._threadRepository.verifyThreadExists(threadId);
+
     // Get thread and comments data from repositories
     const thread = await this._threadRepository.getThreadById(threadId);
     const comments = await this._commentRepository.getCommentsByThreadId(
       threadId
     );
 
-    const commentsList = Array.isArray(comments) ? comments : [];
+    let commentsWithReplies = [];
 
-    // Create CommentDetail instances for each comment
-    const commentDetails = commentsList.map((comment) => {
-      console.log("Comment from repository:", comment);
-      return new CommentDetail({
-        id: comment.id,
-        username: comment.username,
-        date: comment.date,
-        content: comment.content,
-        isDeleted: comment.is_deleted,
+    if (comments.length > 0) {
+      const commentIds = comments.map((comment) => comment.id); // Fix: rawComments -> comments
+      const rawReplies = await this._replyRepository.getRepliesByCommentIds(
+        commentIds
+      );
+
+      commentsWithReplies = comments.map((comment) => {
+        // Fix: rawComments -> comments
+        const commentRepliesData = rawReplies
+          .filter((reply) => reply.comment_id === comment.id)
+          .map(
+            (reply) =>
+              new ReplyDetail({
+                // Map to ReplyDetail entity
+                id: reply.id,
+                content: reply.content,
+                date: reply.date,
+                username: reply.username,
+                isDeleted: reply.is_deleted,
+              })
+          );
+
+        return new CommentDetail({
+          id: comment.id,
+          username: comment.username,
+          date: comment.date,
+          content: comment.content,
+          isDeleted: comment.is_deleted,
+          replies: commentRepliesData,
+        });
       });
+    }
+    return new ThreadDetail({
+      ...thread,
+      comments: commentsWithReplies,
     });
-
-    // Create and return ThreadDetail with all required properties
-    const threadDetail = new ThreadDetail({
-      id: thread.id,
-      title: thread.title,
-      body: thread.body,
-      date: thread.date,
-      username: thread.username,
-      comments: commentDetails,
-    });
-
-    return threadDetail;
   }
 }
 
